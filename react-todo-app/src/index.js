0370da232dee2a10660b61d546bd9589e8b07b85
jmontero-nts <jmontero@neural.com.do>
2022-08-02 19:05:12 -0400

    todolist component file is updated

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -2,5 +2,9 @@ import React from "react"
 import  ReactDOM  from "react-dom"
 
 import TodoContainer from "./components/TodoContainer"
+import TodoList from "./components/TodoList"
 ReactDOM.render(
 <React.StrictMode><TodoContainer/></React.StrictMode>, document.getElementById("root"));
+ReactDOM.render(
+    <TodoList/>, document.getElementById("root.1")
+)
